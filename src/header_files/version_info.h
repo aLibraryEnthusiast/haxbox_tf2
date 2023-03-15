067477972e06cf8711ab2b804267ea556cd7e1fa
aLibraryEnthusiast <alexeason842@gmail.com>
2023-03-15 11:22:28 +0000

    a

--- src/header_files/version_info.h
+++ src/header_files/version_info.h
@@ -1,3 +1,6 @@
 #define name "haxbox"
 #define version "1.0.0"
 #define wlook_updated "https://raw.github.com/aLibraryEnthusiast/haxbox/tf2_updated.txt"
+#define wlook_true "1"
+#define wlook_false "0"
+#define haxbox_version "https://raw.github.com/aLibraryEnthusiast/haxbox/haxbox_version.txt"
